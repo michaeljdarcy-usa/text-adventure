4afc9b715e78c849067665b09ba3b6dc433e5edd
michaeljdarcy <180347967+michaeljdarcy-usa@users.noreply.github.com>
2025-05-17 09:20:23 -0400

    added welcome message and playerName variable

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
+#include <iostream> // Include the I/O stream library for input and output
+
+// The starting point of the program
 int main()
 {
+    // Print, "Hello, world!" to the console
+    std::cout << "Welcome to the Dragon Realm!" << std::endl;
+
+    // Declare a string variable to hold the player's name
+    std::string playerName;
+
+    // Prompt the player for their name
+
+    // Return 0 to indicate successful completion to operating sytem
     return 0;
 }
